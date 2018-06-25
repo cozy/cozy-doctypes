7c2562c9cec555ce9daacaa7ba962c3593a3e441
Patrick Browne <pt.browne@gmail.com>
2018-06-25 15:10:44 +0200

    feat: BankAccount can determine if we have accounts from a new connector

--- src/BankAccount.js
+++ src/BankAccount.js
@@ -13,7 +13,19 @@ class BankAccount extends Document {
       }
     })
   }
+
+  static isFromNewKonnector(fetchedAccounts, stackAccounts) {
+    const byNumber = keyBy(stackAccounts, 'number')
+    const byVendorId = keyBy(stackAccounts, 'vendorId')
+    // We are saving from a new linxo account if we have at least one account
+    // that has a known account number but an unknown linxoId
+    return some(
+      fetchedAccounts,
+      acc => byNumber[acc.accountNumber] && !byVendorId[acc.id]
+    )
+  }
 }
+
 BankAccount.doctype = 'io.cozy.bank.accounts'
 BankAccount.idAttributes = ['_id']
 BankAccount.version = 1
